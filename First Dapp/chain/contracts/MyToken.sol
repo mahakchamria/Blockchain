83924d74991e3ce364930ca4fad18c56bc9188b3
Mahak Chamria <mahakchamria@gmail.com>
2023-06-27 11:54:49 +0530

    Add files via upload

--- First Dapp/chain/contracts/MyToken.sol
+++ First Dapp/chain/contracts/MyToken.sol
@@ -1,1 +1,11 @@
+// SPDX-Licence-Identifier: UNLICENSED
 
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken","MT"){
+        _mint(msg.sender, initialSupply*10**decimals());
+    }
+}
