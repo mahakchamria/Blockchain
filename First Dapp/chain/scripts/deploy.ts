c1cbfea0dd0577146b81fc27db429394d1e7edf8
Mahak Chamria <mahakchamria@gmail.com>
2023-06-27 11:55:35 +0530

    Create deploy.ts

---
+++ First Dapp/chain/scripts/deploy.ts
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+import {ethers}  from "hardhat";
+
+async function main() {
+  const initialSupply = 10000;
+  
+  const myToken = await ethers.deployContract("MyToken",[initialSupply]);
+
+  await myToken.waitForDeployment();
+  const totalSupply = await myToken.totalSupply();
+
+  console.log(`MyToken deployed to ${myToken.target} with an initialSupply ${totalSupply}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
